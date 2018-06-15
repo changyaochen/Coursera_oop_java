1ffd15ae2cb184852524c6c5d6ca227a6453da71
Changyao <changyao.chen@gmail.com>
2018-06-15 18:36:09 -0400

    finish wordplay assignment

--- part_2/WordPlay/WordPlay.java
+++ part_2/WordPlay/WordPlay.java
@@ -14,7 +14,33 @@ public class WordPlay {
         return true;
     }
     
+    public String replaceVowels(String phrase, char ch) {
+        StringBuilder sb = new StringBuilder(phrase);
+        for (int i=0; i<phrase.length(); i++) {
+            if (isVowel(phrase.charAt(i)) == true) {
+                sb.setCharAt(i, ch);
+            }
+        }
+        return sb.toString();
+    }
+    
+    public String emphasize(String phrase, char ch) {
+        StringBuilder sb = new StringBuilder(phrase);
+        for (int i=0; i<phrase.length(); i++) {
+            if (Character.toUpperCase(phrase.charAt(i)) 
+                == Character.toUpperCase(ch)) {
+                if (i%2==0) {  // replace with *
+                    sb.setCharAt(i, '*');
+                }
+                else {
+                    sb.setCharAt(i, '+');
+                }
+            }
+        }
+        return sb.toString();
+    }
+    
     public void test() {
-        System.out.println(isVowel('a'));
+        System.out.println(emphasize("Mary Bella Abracadabra", 'a'));
     }
 }
